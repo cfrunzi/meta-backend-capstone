898db665b5ab1277fb95cd95c31d19342b84cb1f
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-02-12 10:47:56 -0500

    added final unit tests

--- workspace/littlelemon/restaurant/tests.py
+++ workspace/littlelemon/restaurant/tests.py
@@ -4,9 +4,11 @@ from restaurant.models import Menu
 class MenuItemTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(id=4, title="IceCream", price=80, inventory=100)
-        self.assertEqual(item, "IceCream : 80")
+        self.assertEqual(str(item), "IceCream  : 80")
 
 class MenuViewTest(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-    pass
+    def setUp(self):
+        item_one = Menu.objects.create(id=5, title="Gnocchi", price=20, inventory=150)
+        self.assertEqual(str(item_one), "Gnocchi  : 20")
+    def test_getall(self):
+        super(MenuViewTest, self).setUp()
