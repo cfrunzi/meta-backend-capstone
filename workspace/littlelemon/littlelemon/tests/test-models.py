aa2091689ba85a4fe1cbd038f0bbb4934d0b1486
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-02-12 09:35:42 -0500

    deleted tests folder

--- workspace/littlelemon/littlelemon/tests/test-models.py
+++
@@ -1,8 +0,0 @@
-import django.test
-from django.test import TestCase
-from restaurant.models import Menu
-
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(item, "IceCream : 80")
