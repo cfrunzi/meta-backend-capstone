c8ddcf960d9d5db8ad334db9f0926f685cce2aea
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-02-12 08:58:03 -0500

    updated menu model and urls

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 from .views import MenuItemsView, SingleItemMenuView, BookingView, SingleBookingItemView
-from rest_framework.authtoken.views import obtain_auth_token
+#from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleItemMenuView.as_view()),
     path('booking/', views.BookingView.as_view()),
     path('booking/<int:pk>', views.SingleBookingItemView.as_view()),
-    path('api-token-auth/', obtain_auth_token),
+    #path('api-token-auth/', obtain_auth_token), 
+    path('', views.index, name='index'),
 ]
