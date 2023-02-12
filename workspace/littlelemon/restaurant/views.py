98bffde0cc2482f1855c80a12642592cda471963
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-02-12 07:14:41 -0500

    added additional restaurant urls

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def MenuItemView(request):
+    pass
+
+def SingleItemMenuView(request):
+    pass
